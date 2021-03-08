ef270580364120b93254e0811bf0b81954b1d9d8
Seth Onyango <seth.onyango@tingle.software>
2021-03-08 14:50:37 +0300

    Test the creation of evaluations

--- falu/src/test/java/tingle/software/falu/FaluEndToEndTest.kt
+++ falu/src/test/java/tingle/software/falu/FaluEndToEndTest.kt
@@ -1,18 +1,64 @@
 package tingle.software.falu
 
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.nhaarman.mockitokotlin2.argWhere
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import tingle.software.falu.exceptions.FaluException
+import tingle.software.falu.model.EvaluationRequest
+import tingle.software.falu.model.EvaluationResponse
+import tingle.software.falu.model.EvaluationScope
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import kotlin.test.AfterTest
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
-class FaluUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+@RunWith(RobolectricTestRunner::class)
+@ExperimentalCoroutinesApi
+class FaluEndToEndTest {
+
+    private val context: Context = ApplicationProvider.getApplicationContext()
+    private val falu = Falu(FakeKeys.TEST_PUBLISHABLE_KEY)
+
+    private val testDispatcher = TestCoroutineDispatcher()
+
+    @AfterTest
+    fun cleanup() {
+        testDispatcher.cleanupTestCoroutines()
     }
+
+    @Test
+    fun testCreateEvaluationThrowsException() {
+        val createEvalCallback: ApiResultCallback<EvaluationResponse> = mock()
 
         val file = File(context.cacheDir, "falu.pdf")
         val fileStream = context.resources.openRawResource(R.raw.falu)
         copyStreamToFile(fileStream, file)
+
+        val request = EvaluationRequest(
+            scope = EvaluationScope.PERSONAL,
+            name = "JOHN DOE",
+            phone = "+2547123456789",
+            password = "12345678",
+            file = file,
+            description = ""
+        )
+
+        falu.createEvaluation(request, createEvalCallback)
+
+        verify(createEvalCallback).onError(
+            argWhere {
+                it is FaluException
+            }
+        )
+    }
 
     private fun copyStreamToFile(inputStream: InputStream, outputFile: File) {
         inputStream.use { input ->
