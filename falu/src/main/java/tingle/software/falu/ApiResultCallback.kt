045c515dfd4f9294420c858aaf3d4787f31d7946
Seth Onyango <seth.onyango@tingle.software>
2021-03-08 12:21:34 +0300

    Add generic interface for an API operation callback

---
+++ falu/src/main/java/tingle/software/falu/ApiResultCallback.kt
@@ -0,0 +1,10 @@
+package tingle.software.falu
+
+/**
+ * Generic interface for an API operation callback that either returns a
+ * result, [TResult], or an [Exception]
+ */
+interface ApiResultCallback<in TResult> {
+    fun onSuccess(result: TResult)
+    fun onError(e: Exception)
+}
