d0ee4c6c807cd53f4d70b3bb80476ebc394c2c75
Seth Onyango <seth.onyango@tingle.software>
2021-03-08 13:59:44 +0300

    Add a working publishable test key

--- falu/src/test/java/tingle/software/falu/FaluEndToEndTest.kt
+++ falu/src/test/java/tingle/software/falu/FaluEndToEndTest.kt
@@ -4,11 +4,6 @@ package tingle.software.falu
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class FaluUnitTest {
     @Test
     fun addition_isCorrect() {
