1d91dc2d6b48a02b60a47a38a449f39f529c2330
Seth Onyango <seth.onyango@tingle.software>
2021-03-08 14:49:53 +0300

    Copy input stream to file

--- falu/src/test/java/tingle/software/falu/FaluEndToEndTest.kt
+++ falu/src/test/java/tingle/software/falu/FaluEndToEndTest.kt
@@ -11,4 +11,18 @@ class FaluUnitTest {
     }
 
 
+    private fun copyStreamToFile(inputStream: InputStream, outputFile: File) {
+        inputStream.use { input ->
+            val outputStream = FileOutputStream(outputFile)
+            outputStream.use { output ->
+                val buffer = ByteArray(4 * 1024) // buffer size
+                while (true) {
+                    val byteCount = input.read(buffer)
+                    if (byteCount < 0) break
+                    output.write(buffer, 0, byteCount)
+                }
+                output.flush()
+            }
+        }
+    }
 }
