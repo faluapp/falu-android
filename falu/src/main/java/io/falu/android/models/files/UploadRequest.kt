140ec184808b38f31c3cd58c9fa5c28e57595f49
Seth Onyango <sethi.onyango@gmail.com>
2021-09-11 11:53:14 +0300

    Add file upload request and response models

---
+++ falu/src/main/java/io/falu/android/models/files/UploadRequest.kt
@@ -0,0 +1,21 @@
+package io.falu.android.models.files
+
+import java.io.File
+import java.util.*
+
+data class UploadRequest(
+    /**
+     * Contents of the file. It should follow the specifications of RFC 2388.
+     */
+    var file: File,
+
+    /**
+     * Purpose for a file
+     */
+    var purpose: FilePurpose,
+    /**
+     * Time at which the file expires.
+     */
+    var date: Date? = null,
+    var description: String? = null
+)
