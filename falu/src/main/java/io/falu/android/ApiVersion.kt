7a7f16be43200f9b6cce49495e9a816885cbd34b
Seth Onyango <sethi.onyango@gmail.com>
2022-05-26 08:05:58 +0300

    Update the target api version

--- falu/src/main/java/io/falu/android/ApiVersion.kt
+++ falu/src/main/java/io/falu/android/ApiVersion.kt
@@ -15,7 +15,7 @@ internal data class ApiVersion internal constructor(internal val version: String
         get() = version
 
     internal companion object {
-        private const val API_VERSION_CODE: String = "2021-09-03"
+        private const val API_VERSION_CODE: String = "2022-01-01"
 
         private val INSTANCE = ApiVersion(API_VERSION_CODE)
 
