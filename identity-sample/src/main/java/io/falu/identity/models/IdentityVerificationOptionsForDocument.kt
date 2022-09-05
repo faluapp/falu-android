71f0e16a27a1d15760f1612e897240337c00eed0
Seth Onyango <sethi.onyango@gmail.com>
2022-09-05 12:44:03 +0300

    Update identity verification

--- identity-sample/src/main/java/io/falu/identity/models/IdentityVerificationOptionsForDocument.kt
+++ identity-sample/src/main/java/io/falu/identity/models/IdentityVerificationOptionsForDocument.kt
@@ -2,11 +2,6 @@ package io.falu.identity.models
 
 data class IdentityVerificationOptionsForDocument(
     /**
-     * Disable image uploads, identity document images have to be captured using the device's camera.
-     */
-    var live: Boolean? = null,
-
-    /**
      * The allowed identity document types. If a user uploads a document which isn't one of the allowed types, it will be rejected.
      */
     var allowed: MutableList<String>?
