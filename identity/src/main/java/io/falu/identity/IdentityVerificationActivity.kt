14d8e20a529e8e13d315766b2270f7e7eaf123c1
Seth Onyango <sethi.onyango@gmail.com>
2022-09-08 07:25:30 +0300

    Create welcome page for identity verification

---
+++ identity/src/main/java/io/falu/identity/IdentityVerificationActivity.kt
@@ -0,0 +1,11 @@
+package io.falu.identity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IdentityVerificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_identity_verification)
+    }
+}
