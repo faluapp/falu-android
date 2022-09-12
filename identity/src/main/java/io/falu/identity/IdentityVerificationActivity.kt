03720702e1882623a775f58a2bbf9fac70c9551a
Seth Onyango <sethi.onyango@gmail.com>
2022-09-12 08:18:54 +0300

    Remove app toolbar from sdk

--- identity/src/main/java/io/falu/identity/IdentityVerificationActivity.kt
+++ identity/src/main/java/io/falu/identity/IdentityVerificationActivity.kt
@@ -1,10 +1,12 @@
 package io.falu.identity
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import io.falu.identity.databinding.ActivityIdentityVerificationBinding
 
 internal class IdentityVerificationActivity : AppCompatActivity() {
+    private val verificationViewModel: IdentityVerificationViewModel by viewModels()
 
     private val binding by lazy {
         ActivityIdentityVerificationBinding.inflate(layoutInflater)
