c6bc94e567a732ed4c190818c97612f6dd29e246
Seth Onyango <sethi.onyango@gmail.com>
2022-09-26 18:02:04 +0300

    Launch camera view to take a photo

--- identity/src/main/java/io/falu/identity/capture/CaptureDocumentViewModel.kt
+++ identity/src/main/java/io/falu/identity/capture/CaptureDocumentViewModel.kt
@@ -1,11 +1,15 @@
 package io.falu.identity.capture
 
+import android.content.Context
 import android.net.Uri
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
+import io.falu.identity.utils.FileUtils
 
-class CaptureDocumentViewModel() : ViewModel() {
+internal class CaptureDocumentViewModel() : ViewModel() {
 
+    private lateinit var imageCaptureFront: ImageCapture
+    private lateinit var imageCaptureBack: ImageCapture
     private lateinit var frontImagePicker: ImagePicker
     private lateinit var backImagePicker: ImagePicker
 
@@ -14,11 +18,38 @@ class CaptureDocumentViewModel() : ViewModel() {
      */
     fun captureDocumentImages(
         fragment: Fragment,
+        fileUtils: FileUtils,
+        onFrontImageCaptured: (Uri) -> Unit,
+        onBackImageCaptured: (Uri) -> Unit
+    ) {
+        imageCaptureFront = ImageCapture(fragment, fileUtils, onFrontImageCaptured)
+        imageCaptureBack = ImageCapture(fragment, fileUtils, onBackImageCaptured)
+    }
+
+    /**
+     *
+     */
+    fun pickDocumentImages(
+        fragment: Fragment,
         onFrontImagePicked: (Uri) -> Unit,
         onBackImagePicked: (Uri) -> Unit
     ) {
         frontImagePicker = ImagePicker(fragment, onFrontImagePicked)
         backImagePicker = ImagePicker(fragment, onBackImagePicked)
+    }
+
+    /**
+     *
+     */
+    fun captureImageFront(context: Context) {
+        imageCaptureFront.captureImage(context)
+    }
+
+    /**
+     *
+     */
+    fun captureImageBack(context: Context) {
+        imageCaptureBack.captureImage(context)
     }
 
     /**
