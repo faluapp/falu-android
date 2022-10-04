12eb8c42fd96b2787a86c870da06ff13237ffad4
Seth Onyango <sethi.onyango@gmail.com>
2022-10-04 12:36:23 +0300

    Setup business info on Welcome fragment

--- identity/src/main/java/io/falu/identity/welcome/WelcomeFragment.kt
+++ identity/src/main/java/io/falu/identity/welcome/WelcomeFragment.kt
@@ -1,18 +1,25 @@
 package io.falu.identity.welcome
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import io.falu.identity.IdentityVerificationViewModel
 import io.falu.identity.R
+import io.falu.identity.api.models.Verification
 import io.falu.identity.databinding.FragmentWelcomeBinding
+import software.tingle.api.HttpApiResponseProblem
 
 class WelcomeFragment : Fragment() {
 
     private var _binding: FragmentWelcomeBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: IdentityVerificationViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,10 +32,34 @@ class WelcomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel.observeForVerificationResults(
+            viewLifecycleOwner,
+            onSuccess = { onVerificationSuccessful(it) },
+            onFailure = { onVerificationFailure(it) })
+
+
+        binding.tvWelcomeBody.movementMethod = LinkMovementMethod.getInstance()
 
         binding.buttonGetStarted.setOnClickListener {
             findNavController().navigate(R.id.action_fragment_welcome_to_fragment_document_selection)
         }
+    }
+
+    private fun onVerificationSuccessful(verification: Verification) {
+        binding.buttonGetStarted.isEnabled = true
+        binding.tvWelcomeSubtitle.text =
+            getString(R.string.welcome_subtitle, verification.workspace.name)
+        binding.tvWelcomeBody.text = getString(
+            R.string.welcome_body,
+            verification.business.name,
+        )
+        binding.tvWelcomeBody.movementMethod = LinkMovementMethod.getInstance()
+
+    }
+
+    private fun onVerificationFailure(error: HttpApiResponseProblem?) {
+        binding.buttonGetStarted.isEnabled = false
+        // TODO: Redirect to error fragment
     }
 
     override fun onDestroyView() {
