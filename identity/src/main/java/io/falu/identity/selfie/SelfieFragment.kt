78c8fed4eb2bc492e161d286b2b7d77a7ee6666b
Seth Onyango <sethi.onyango@gmail.com>
2022-10-28 07:57:49 +0300

    Create selfie fragment

---
+++ identity/src/main/java/io/falu/identity/selfie/SelfieFragment.kt
@@ -0,0 +1,35 @@
+package io.falu.identity.selfie
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import io.falu.identity.IdentityVerificationViewModel
+import io.falu.identity.databinding.FragmentSelfieBinding
+
+class SelfieFragment : Fragment() {
+    private val viewModel: IdentityVerificationViewModel by activityViewModels()
+
+    private var _binding: FragmentSelfieBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSelfieBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
