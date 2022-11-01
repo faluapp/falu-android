1631fb37dc48a84a1cae89f18ec4a65784c79147
Seth Onyango <sethi.onyango@gmail.com>
2022-11-01 10:57:36 +0300

    Upload selfie image and submit verification documents

--- identity/src/main/java/io/falu/identity/api/models/verification/VerificationUploadRequest.kt
+++ identity/src/main/java/io/falu/identity/api/models/verification/VerificationUploadRequest.kt
@@ -1,6 +1,10 @@
 package io.falu.identity.api.models.verification
 
+import android.content.Intent
+import android.os.Bundle
 import android.os.Parcelable
+import androidx.core.os.bundleOf
+import io.falu.identity.IdentityVerificationResult
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -9,4 +13,20 @@ internal data class VerificationUploadRequest(
     var country: String? = null,
     var document: VerificationDocumentUpload,
     var selfie: VerificationSelfieUpload? = null
-) : Parcelable
+) : Parcelable {
+
+    @JvmSynthetic
+    fun addToBundle() = bundleOf(KEY_VERIFICATION_UPLOAD_REQUEST to this)
+
+    internal companion object {
+        private const val KEY_VERIFICATION_UPLOAD_REQUEST = ":verification"
+
+        fun getFromBundle(bundle: Bundle?): VerificationUploadRequest? {
+            return bundle?.getParcelable(KEY_VERIFICATION_UPLOAD_REQUEST)
+        }
+
+        fun getFromIntent(intent: Intent?): IdentityVerificationResult? {
+            return intent?.getParcelableExtra(KEY_VERIFICATION_UPLOAD_REQUEST)
+        }
+    }
+}
--- identity/src/main/java/io/falu/identity/selfie/SelfieFragment.kt
+++ identity/src/main/java/io/falu/identity/selfie/SelfieFragment.kt
@@ -8,17 +8,29 @@ import android.view.ViewGroup
 import androidx.camera.core.CameraSelector
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import io.falu.core.models.FaluFile
 import io.falu.identity.IdentityVerificationViewModel
 import io.falu.identity.R
+import io.falu.identity.api.models.CameraLens
+import io.falu.identity.api.models.CameraSettings
+import io.falu.identity.api.models.Exposure
+import io.falu.identity.api.models.UploadMethod
+import io.falu.identity.api.models.verification.VerificationSelfieUpload
+import io.falu.identity.api.models.verification.VerificationUploadRequest
 import io.falu.identity.databinding.FragmentSelfieBinding
 import io.falu.identity.utils.navigateToApiResponseProblemFragment
 import io.falu.identity.utils.navigateToErrorFragment
+import io.falu.identity.utils.submitVerificationData
+import io.falu.identity.utils.updateVerification
+import software.tingle.api.patch.JsonPatchDocument
 
 class SelfieFragment : Fragment() {
-    private val viewModel: IdentityVerificationViewModel by activityViewModels()
+    private val identityViewModel: IdentityVerificationViewModel by activityViewModels()
 
     private var _binding: FragmentSelfieBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var verificationRequest: VerificationUploadRequest
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,11 +43,14 @@ class SelfieFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        verificationRequest =
+            requireNotNull(VerificationUploadRequest.getFromBundle(requireArguments())) {
+                "Verification upload request is null"
+            }
+
         binding.viewCamera.lifecycleOwner = viewLifecycleOwner
         binding.viewCamera.lensFacing = CameraSelector.LENS_FACING_FRONT
-
         binding.buttonContinue.text = getString(R.string.button_continue)
-        binding.buttonContinue.isEnabled = false
 
         binding.buttonTakeSelfie.setOnClickListener {
             binding.viewCamera.takePhoto(
@@ -59,21 +74,46 @@ class SelfieFragment : Fragment() {
         binding.ivSelfie.setImageURI(selfieUri)
 
         binding.buttonContinue.setOnClickListener {
+            binding.buttonContinue.showProgress()
             uploadSelfie(selfieUri)
         }
     }
 
     private fun uploadSelfie(uri: Uri) {
-        viewModel.uploadSelfieImage(
+        identityViewModel.uploadSelfieImage(
             uri,
+            onSuccess = { submitSelfieAndUploadedDocuments(it) },
             onFailure = { navigateToErrorFragment(it) },
             onError = { navigateToApiResponseProblemFragment(it) },
         )
     }
 
-    private fun submitSelfieAndUploadedDocuments() {
+    private fun submitSelfieAndUploadedDocuments(file: FaluFile) {
+        val selfie =
+            VerificationSelfieUpload(
+                UploadMethod.MANUAL,
+                file = file.id,
+                variance = 0F,
+                camera = cameraSettings
+            )
 
+        val document = JsonPatchDocument().replace("/selfie", selfie)
+
+        updateVerification(identityViewModel, document, R.id.fragment_selfie, onSuccess = {
+            verificationRequest.selfie = selfie
+            submitVerificationData(identityViewModel, R.id.fragment_selfie, verificationRequest)
+        })
     }
+    
+    private val cameraSettings: CameraSettings
+        get() {
+            val info = binding.viewCamera.cameraInfo
+            return CameraSettings(
+                lens = CameraLens(model = "", focalLength = 0F),
+                brightness = 0F,
+                exposure = Exposure(iso = 0F, duration = 0F)
+            )
+        }
 
     override fun onDestroyView() {
         super.onDestroyView()
